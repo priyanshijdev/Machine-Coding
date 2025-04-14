6ebdf3d10ad7a2b60905bed7e6a2babd496c121f
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-04-14 21:28:20 +0530

    added debouncing concept -optimising

--- Autocomplete SearchBar/index.tsx
+++ Autocomplete SearchBar/index.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import "./styles.css";
 
-// auto complete search
+// auto complete search bar ---
 // ask where will we get data dump?
+// also onblur and onfocus features
 export default function App() {
   const [result, setResult] = useState([]);
   const [input, setInput] = useState("");
+  const [showResult, setShowResult] = useState(false);
 
   // function for api call
   const fetchData = async () => {
+    console.log(" heyyyy", input);
     const data = await fetch("https://dummyjson.com/recipes/search?q=" + input);
     const json = await data.json();
     setResult(json?.recipes);
@@ -16,6 +19,13 @@ export default function App() {
 
   useEffect(() => {
     fetchData();
+    // implement debounding -> it will not fecth data on every key stroke
+    // when user pause then only apu call were made .
+    const timer = setTimeout(fetchData, 300);
+    // unmounting
+    return () => {
+      clearTimeout(timer);
+    };
   }, [input]);
 
   return (
@@ -26,13 +36,16 @@ export default function App() {
         className="search-input"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onFocus={() => setShowResult(true)}
+        onBlur={() => setShowResult(false)}
       />
       <div className="result-container">
-        {result.map((row: any) => (
-          <span className="result" key={row.id}>
-            {row?.name}
-          </span>
-        ))}
+        {showResult &&
+          result.map((row: any) => (
+            <span className="result" key={row.id}>
+              {row?.name}
+            </span>
+          ))}
       </div>
     </div>
   );
