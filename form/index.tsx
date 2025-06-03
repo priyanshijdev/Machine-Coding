34e349e949d90523300d94bb0c7b6218c356936e
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-06-03 21:56:07 +0530

    Create index.tsx

---
+++ form/index.tsx
@@ -0,0 +1,16 @@
+// Form with Validation
+// . Form with Validation
+// Example: Create User Form
+
+// Fields: name, email, phone, address.
+// Validate required fields, email format, phone digits.
+// Submit to dummy API.
+// Show success or error.
+
+
+import React from "react";
+
+export const Form = () => {
+
+  return <>hi form</>;
+};
