ac7b1bb51e0eaaeebdec33217ff2eb4502590157
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-06-05 14:00:11 +0530

    Create index.tsx

---
+++ SearchBar/index.tsx
@@ -0,0 +1,8 @@
+// 2. Product List with Search and Pagination
+// Requirements:
+// Fetch product list from mock API (or dummy JSON).
+// Search by product name.
+// Paginate results---------------------------------
+
+
+
