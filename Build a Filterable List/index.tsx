87efc3e0ddaa5251611aa4ac11088b165197aa57
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-06-13 15:45:24 +0530

    Create index.tsx

---
+++ Build a Filterable List/index.tsx
@@ -0,0 +1,9 @@
+ Title: Build a Filterable User List
+
+Problem Statement:
+Build a simple React app that displays a list of users and allows the user to:
+
+Search by name using a text input.
+
+Filter by age group using a dropdown (e.g., "All", "Below 25", "25-40", "Above 40").
+
