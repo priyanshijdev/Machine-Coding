e774c4f21aa2aec6f1c1710188ef32c7e63062b7
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-07-26 15:39:42 +0530

    Create index.js

---
+++ Holy Grail/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <header>Header </header>
+      <div className="content">
+        <nav> Navbar </nav>
+        <main> Content </main>
+        <aside> Sidebar </aside>
+      </div>
+      <footer> Footer </footer>
+    </>
+  );
+}
