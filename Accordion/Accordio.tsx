ab73520e1c4d579e648601fee97ebea578155bf3
Priyanshi Jain <91975234+priyanshijdev@users.noreply.github.com>
2025-08-17 22:10:21 +0530

    Update Accordio.tsx

--- Accordion/Accordio.tsx
+++ Accordion/Accordio.tsx
@@ -1,34 +1,37 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 
 interface AccordionProps {
   title: string;
   children: React.ReactNode;
 }
-
-const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
+const Accordion = ({ title, children }: AccordionProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div
-      style={{
-        border: "1px solid #ccc",
-        marginBottom: "8px",
-        borderRadius: "4px",
-      }}
-    >
+    <>
       <div
         style={{
-          background: "#f5f5f5",
-          padding: "10px",
-          cursor: "pointer",
-          fontWeight: "bold",
+          border: "1px solid #ccc",
+          marginBottom: "8px",
+          borderRadius: "4px",
         }}
-        onClick={() => setIsOpen(!isOpen)}
       >
-        {title}
+        <div
+          style={{
+            padding: "10px",
+            backgroundColor: "gray",
+            color: "black",
+            cursor: "pointer",
+            fontWeight: "bold",
+          }}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {" "}
+          {title}
+        </div>
+        {isOpen && <div style={{ padding: "8px" }}> {children}</div>}
       </div>
-      {isOpen && <div style={{ padding: "10px" }}>{children}</div>}
-    </div>
+    </>
   );
 };
 
